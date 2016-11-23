46359b20d54fe15ad30e73e2ecced89d39c2cf37
Landon West <landonwest5@gmail.com>
2016-11-23 11:38:28 -0800

    parse the request and send the request parameters to the Rack app

--- http_server.rb
+++ http_server.rb
@@ -9,29 +9,28 @@ server = TCPServer.new 5678
 while session = server.accept
   request = session.gets
   puts request
+  # split the request string into a method and a full path
+  method, full_path = request.split(' ')
+  # split the full path into a path and a query
+  path, query = full_path.split('?')
+  # pass those to our app in a Rack environment hash
+  status, headers, body = app.call({
+    'REQUEST_METHOD' => method,
+    'PATH_INFO' => path,
+    'QUERY_STRING' => query
+  })
 
-  # 1
-  status, headers, body = app.call({})
-  # 2
   session.print "HTTP/1.1 #{status}\r\n"
-  # 3
+
   headers.each do |key, value|
     session.print "#{key}: #{value}\r\n"
   end
-  # 4
+
   session.print "\r\n"
-  # 5
+
   body.each do |part|
     session.print part
   end
 
   session.close
-
-  # session.print "HTTP/1.1 200\r\n" # 1 status line
-  # session.print "Content-Type: text/html\r\n" # 2 header
-  # session.print "\r\n" # 3 newline
-  # session.print "Hi there! I am your Ruby server! The time is
-  #   #{Time.now.strftime("%I:%M:%S %p")}" #4 body
-
-  # session.close
 end
